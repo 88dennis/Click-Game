00745e15e933a09ed8547c4796865b413bcd177b
Dennis <dennissarmiento808@gmail.com>
2019-04-26 10:17:32 -0700

    Updates

---
+++ precache-manifest.b2a4d11631aa88a3531f75eedeeda871.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a1da1345a285117ba74890d69d0478",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "3ca0542324f9f240c2ec",
+    "url": "/click-game/static/css/main.8a965d60.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/click-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "3ca0542324f9f240c2ec",
+    "url": "/click-game/static/js/main.f8d94f11.chunk.js"
+  },
+  {
+    "revision": "aa765b97c1e7aaa53c3e",
+    "url": "/click-game/static/js/runtime~main.7d043b49.js"
+  }
+]);
