d7038c1a0b123c6fa4ea3fbf02f6478467e4ff73
Dennis <dennissarmiento808@gmail.com>
2019-04-26 20:21:49 -0700

    up

--- src/App.js
+++ src/App.js
@@ -2,44 +2,71 @@ import React, { Component } from "react";
 import VillainCard from "./components/VillainCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./images.json";
+import villains from "./images.json";
+
+console.log(villains)
+
+
+// function randomize(arr){
+//   for (let i = arr.length - 1; i > 0; i--) {
+//          const j = Math.floor(Math.random() * (i + 1));
+//          [arr[i], arr[j]] = [arr[j], arr[i]];
+//      }
+      
+// }
+// let newArr = randomize(villains)
+
+// console.log(newArr)
 
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
-    friends
+    villains,
+    clicked: false,
+    score: 0,
+    losses: 0,
+    guesses: 0
   };
 
-  removeFriend = id => {
-    // Filter this.state.friends for friends with an id not equal to the id being removed
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ friends });
-  };
+
+  // removeFriend = id => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const villains = this.state.villains.filter(villain => villain.id !== id);
+  //   // Set this.state.friends equal to the new friends array
+  //   this.setState({ villains });  
+  // };
+
+
+  // Randomizer = ()) => {
+  //   // Filter this.state.friends for friends with an id not equal to the id being removed
+  //   const newvillains = this.state.villains;
+
+
+  //    
+  // };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
+
     return (
       <Wrapper>
         <Title>Clicky Game</Title>
-        {this.state.friends.map(friend => (
+
+        {this.state.villains.map(villain => (
+          
           <VillainCard
             removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            id={villain.id}
+            key={villain.id}
+            name={villain.name}
+            image={villain.image}
           />
+          
         ))}
       </Wrapper>
     );
   }
 }
-
-
-
 
 
 export default App;
--- src/components/VillainCard/index.js
+++ src/components/VillainCard/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+// function randomizedVillains(villainsArr){
+//   console.log(villainsArr)
+//  for (let i = villainsArr.length - 1; i > 0; i--) {
+//      const j = Math.floor(Math.random() * (i + 1));
+//      [villainsArr[i], villainsArr[j]] = [villainsArr[j], villainsArr[i]];
+//  }
+ 
+// };
+// let randomprops = randomizedVillains(props);
+
+function VillainCard(props) {
+  console.log("Dennis", props)
+   
+
   return (
     <div className="card">
       <div className="img-container fade">
-        <img alt={props.name} src={props.image} onClick={() => props.removeFriend(props.id)} />
-        
+
+        <img 
+        alt={props.name} 
+        src={props.image} 
+        onClick={() => props.randomize(props)} />
+
       </div>
-
-
-        
 
     </div>
   );
@@ -19,4 +33,5 @@ function FriendCard(props) {
 
 
 
-export default FriendCard;
+
+export default VillainCard;
